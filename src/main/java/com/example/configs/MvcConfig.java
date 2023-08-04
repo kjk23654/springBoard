f9e63cb01750e4cbe46bd95b7f8326340c328720
kjk23654 <kjk23654@naver.com>
2023-08-04 23:50:35 +0900

    템플릿 레이아웃 프론트 완료

--- src/main/java/com/example/configs/MvcConfig.java
+++ src/main/java/com/example/configs/MvcConfig.java
@@ -1,18 +1,24 @@
 package com.example.configs;
 
+import com.example.configs.interceptors.SiteConfigInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+
+
 
     @Value("${file.upload.path}")
     private String fileUploadPath;
+
+    // 사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) { // 정적인 경로를 설정할 때
         registry.addResourceHandler("/uploads/**")
@@ -21,6 +27,13 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("main/index");
+        registry.addViewController("/")
+                .setViewName("main/index");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
     }
 }
