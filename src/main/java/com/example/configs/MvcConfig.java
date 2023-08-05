2beac2141d7b598d24a247e0b9ac7010e9a11429
kjk23654 <kjk23654@naver.com>
2023-08-05 10:28:07 +0900

    회원 가입 엔티티 작성 완료

--- src/main/java/com/example/configs/MvcConfig.java
+++ src/main/java/com/example/configs/MvcConfig.java
@@ -5,10 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaAuditing
 public class MvcConfig implements WebMvcConfigurer {
 
 
